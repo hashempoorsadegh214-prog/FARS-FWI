7f6e9de31e98c319ccc1f1dcc6fdc5e7f0ac6513
hashempoorsadegh214-prog <hashempoor.sadegh214@gmail.com>
2026-08-21 17:37:09 +0330

    Update test_getfeatureinfo.py

--- test_getfeatureinfo.py
+++ test_getfeatureinfo.py
@@ -1,18 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 URL = "https://forest-fire.emergency.copernicus.eu/apps/gwis_current_situation/"
 
 print("=" * 50)
-print("EFFIS VIEWER TEST")
+print("EFFIS JAVASCRIPT FILES")
 print("=" * 50)
 
 response = requests.get(URL, timeout=60)
 
 print("HTTP:", response.status_code)
-print("CONTENT TYPE:", response.headers.get("content-type"))
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+for script in soup.find_all("script", src=True):
+    url = urljoin(URL, script["src"])
+    print(url)
 
 print("=" * 50)
-print("HTML")
+print("END")
 print("=" * 50)
-
-print(response.text[:20000])
